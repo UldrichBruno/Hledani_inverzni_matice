b6d08ffad0751610c1731270ccb2c4411a760f76
UldrichBruno <uldrich.bruno@seznam.cz>
2020-03-01 13:44:51 +0100

    TODO: Make DIM a constant.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,72 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
+double x;
+
+
+
+void print_matrix(double matrix[DIM][DIM]);
+void Row_Reduce(double matrix[DIM][DIM]);
+
+void print_matrix(double matrix[DIM][DIM]) // Outputs the matrix
+{
+
+    for (int i=0; i<DIM; i++) {
+        for (int j=0; j<DIM; j++) {
+            cout << setw(7) << setprecision(3) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+/*void Row_Reduce(double matrix[DIM][DIM]) {
+     int nrows = DIM; // number of rows
+     int ncols = DIM; // number of columns
+
+    int lead = 0;
+
+    while (lead < nrows) {
+        double d, m;
+
+        for (int r = 0; r < nrows; r++) { // for each row ...*/
+            /* calculate divisor and multiplier */
+           /* d = matrix[lead][lead];
+            m = matrix[r][lead] / matrix[lead][lead];
+
+            for (int c = 0; c < ncols; c++) { // for each column ...
+                if (r == lead)
+                    matrix[r][c] /= d;               // make pivot = 1
+                else
+                    matrix[r][c] -= matrix[lead][c] * m;  // make other = 0
+            }
+        }
+
+        lead++;
+    //    print_matrix(matrix);
+    }*/
+
+
+/*int col_elim(int i){
+    for (int k=0;k<i;k++){
+        for (int l=0;l<i;l++){
+
+        }
+    }
+}
+*/
+
+
 
 int main() {
     int DIM;
-    cout << "Input dimension of square matrix"<<endl;
+    double matrix[DIM][DIM];
+    cout << "Input dimension of square matrix" << endl;
     cin >> DIM;
-    int matrix[DIM][DIM];
-    int x;
     ifstream inFile;
 
     inFile.open("C:\\Users\\Uzivatel\\CLionProjects3\\Hledani_inverzni_matice\\matrix.txt");
@@ -17,24 +74,28 @@ int main() {
         cout << "Unable to open file";
         exit(1); // terminate with error
     }
+    while (inFile >> x) {
 
-        while (inFile >> x) {
-            for (int i=0;i<DIM;i++){
-                for (int j=0;j<DIM;j++){
-                   matrix[i][j] = x;
-                    inFile >> x;
-                }
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                matrix[i][j] = x;
+                inFile >> x;
             }
+        }
 
+    }
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
+            cout << matrix[i][j] << " ";
         }
-    for (int i=0;i<DIM;i++){
-        for (int j=0;j<DIM;j++){
-           cout << matrix[i][j] << " ";
-        }
-        cout <<endl;
+        cout << endl;
     }
 
     inFile.close();
-        return 0;
-}
 
+    void print_matrix(double matrix[DIM][DIM]);
+    //void Row_Reduce(double matrix[DIM][DIM]);
+   // void print_matrix(double matrix[DIM][DIM]);
+
+return 0;
+}
